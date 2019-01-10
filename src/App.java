49f4534ed1b0608ffc33229af642b76e55bca103
Roman <lytvynenko.r.i@gmail.com>
2019-01-10 14:30:46 +0200

    improve CursiveNumber

--- src/App.java
+++ src/App.java
@@ -2,7 +2,6 @@ import number.CursiveNumber;
 
 public class App {
     public static void main(String[] args) {
-        CursiveNumber cursiveNumber = new CursiveNumber(768);
-        cursiveNumber.divide();
+         new CursiveNumber(100);
     }
 }
